1f75f0e5c0de4b54d6d160ba621423696b31da81
PhiS <926151+Phi-S@users.noreply.github.com>
2019-07-05 11:12:46 +0200

    Update test.java
    added examples for reboot, setAlias, getAPs and getAPsKeys.

--- test.java
+++ test.java
@@ -1,18 +1,38 @@
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class test {
 
 	public static void main(String[] args) {
 
-		HS1x0api client = new HS1x0api("10.10.2.251", 9999);
+		HS1x0api client = new HS1x0api("10.10.4.220", 9999);
 
+		System.out.println(Arrays.toString(client.getAPs()));
+		
+		List<String[]> erg = client.getAPsKeys();
+		for (int i = 0; i < erg.size(); i++) {
+			System.out.println(Arrays.toString(erg.get(i)));
+		}
+		
 		System.out.println(Arrays.toString(client.getEnergy()));
-		System.out.println(Arrays.toString(client.getSysInfo()));
+		
 		
 		System.out.println(client.getTime());
 		System.out.println(client.setTimezone(new Date()));
 		System.out.println(client.getTime());
 		System.out.println(client.LED(false));	
+		
+		System.out.println(Arrays.toString(client.getEnergy()));
+		
+
+		System.out.println(client.LED(true));	
+		System.out.println(client.LED(false));	
+		
+		System.out.println(Arrays.toString(client.getSysInfo()));
+		System.out.println(client.setAlias("ThePlug"));
+		System.out.println(Arrays.toString(client.getSysInfo()));
+				
+		System.out.println(client.reboot());
 	}
 }
